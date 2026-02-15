024801226e3a3a6f65f4669ebed77251376e77aa
brigdethe <mannisomari@gmail.com>
2026-02-14 20:43:07 -0600

    Admin dashboard

--- app.js
+++ app.js
@@ -10,9 +10,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (_req, res) => res.render('pages/home'));
 app.get('/contact', (_req, res) => res.render('pages/contact'));
 
-app.use(express.static(__dirname, { index: false }));
+app.use(express.static(path.join(__dirname, 'public'), { index: false }));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
